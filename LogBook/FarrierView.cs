720116c59c47740003b713807c7ee9aacf36d208
KRKroening <kimberlykroening@hotmail.com>
2017-03-15 11:02:11 -0600

    Update FarrierView.cs

--- LogBook/FarrierView.cs
+++ LogBook/FarrierView.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,19 +9,20 @@ namespace LogBook
 {
     class FarrierView
     {
-        static string profileName
+        static string activeProfile
         {
             get { return MainWindow.activeProfile; }
         }
         string dirPathName
         {
-            get { return loadExistingProfile.dirPathName; }
+            get { return MainWindow.dirPathName; }
         }
 
         public void saveEditFarrierDemos(List<string> dataToSave)
         {
             List<string[]> openedFile = new List<string[]>();
-            var openFarrierStream = new StreamReader(File.OpenRead(dirPathName));
+            var openFarrierStream = new StreamReader(File.OpenRead(dirPathName + activeProfile + @"\" + activeProfile + "farrier.csv"));
+            
             int count = 0;
             while (!openFarrierStream.EndOfStream)
             {
@@ -30,8 +31,30 @@ namespace LogBook
                 openedFile.Add(lines);
                 count++;
             }
+            count = 0;
+            foreach (string item in dataToSave)
+            {
+                openedFile[0][count] = item;
+                count++;
+            }
+            string finalToWrite = "";
+            foreach (string[] item in openedFile)
+            {
+                foreach (string subitem in item)
+                {
+                    finalToWrite += subitem + ";";
+                }
+                finalToWrite += Environment.NewLine;
+            }
+            openFarrierStream.Close();
+            File.WriteAllText(dirPathName + activeProfile + @"\" + activeProfile + "farrier.csv", finalToWrite);
+
         }
 
+        public void addNewFarrierDate(string newDateToAdd)
+        {
+            File.AppendAllText(dirPathName + activeProfile + @"\" + activeProfile + "farrier.csv", newDateToAdd + ";content" + Environment.NewLine);
+        }
 
     }
 }
